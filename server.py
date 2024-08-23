5087405a04b4bab36a23b291b8880f256e2ecc66
Gune Ko <93659082+Koko1324@users.noreply.github.com>
2024-08-24 00:04:09 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask
+import random 
+app = Flask(__name__)
+
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
+]
+
+
+def template(contsnts, contant):
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {contents}
+            </ol>
+            {content}
+        </body>
+    </html>
+    '''
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+
+@app.route('/')
+def index():
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
+@app.route('/read/<int:id>/')
+def read(id):
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    print(title, body)
+    return template(getContents(),f'<h2>{title}<h2>{body}')
+
+
+app.run(debug=True)
